95d7b029e36312033a9cd825d1842c469dc14903
Batool Malkawi <batoolmalkawii@gmail.com>
2021-01-25 19:31:14 +0200

    initial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+import Todo from './components/todo'
+
+export default function Home() {
+  return (
+    <div>
+        <Todo />
+    </div>
+  )
+}
